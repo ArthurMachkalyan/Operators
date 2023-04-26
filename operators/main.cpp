51b54d0842f4b96e30214075c4d2c4e2f8a0924e
Machk_yz84@top-academy.ru <Machk_yz84@top-academy.ru>
2023-04-26 15:01:44 +0300

    Add project files.

---
+++ operators/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "russian");
+	int a, b;
+	a = 2;
+	b = 3;
+	cout << a << "\t" << b << endl;
+	cout << endl;
+	a = 3;
+	b = 2;
+	cout << a << "\n" << endl << b << endl;
+		
+}
